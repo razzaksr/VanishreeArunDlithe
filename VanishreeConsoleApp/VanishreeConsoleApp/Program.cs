955d2754726c82eab82f5539954d520da1a9046e
razzaksr <razzaksr@gmail.com>
2021-08-03 10:31:41 +0530

    console begin

---
+++ VanishreeConsoleApp/VanishreeConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VanishreeConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

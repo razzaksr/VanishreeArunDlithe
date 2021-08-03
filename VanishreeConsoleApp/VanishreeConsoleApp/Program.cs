4395d1c43e221888035195d995028306c388c354
razzaksr <razzaksr@gmail.com>
2021-08-03 10:37:10 +0530

    console first change

--- VanishreeConsoleApp/VanishreeConsoleApp/Program.cs
+++ VanishreeConsoleApp/VanishreeConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace VanishreeConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Dot net bootcamp welcomes you!");
         }
     }
 }
